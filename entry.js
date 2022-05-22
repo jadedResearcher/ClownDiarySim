ec1823749f7c9af427272e6cd0161a518a7a5f51
JR <jadedResearcher@gmail.com>
2022-05-21 23:28:14 -0400

    styled but needs date calculation and content

---
+++ entry.js
@@ -0,0 +1,17 @@
+//has html text, a title, a date, a word of the day,an icon, and a name. 
+//icon and name will be the same every time, so hardcoded
+class Entry {
+    title = "PLACEHOLDER";
+    date = 0; //relative to the first day of the last even month
+    name = "yongiki";
+    icon = "clown.png";
+    words_of_the_day = "wings,irony";
+
+    constructor(title, text, date, words_of_the_day) {
+        this.title = title;
+        this.text = text;
+        this.date = date;
+        this.words_of_the_day = words_of_the_day;
+    }
+
+}
