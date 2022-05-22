07150cd41d4d56caeaf39c880b77b0dd9c7fa2e3
JR <jadedResearcher@gmail.com>
2022-05-22 11:43:28 -0400

    simple test for dates

--- entries.js
+++ entries.js
@@ -1,5 +1,8 @@
 const entries = [
     new Entry(0,"test1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh lectus, aliquam id ipsum et, tempor commodo magna. Nam non mi felis. Etiam dictum vulputate blandit. Integer ac urna quis lorem dictum porttitor in sed ligula. Morbi eget risus non mauris volutpat tempor. Etiam nec venenatis tellus. Quisque quis dui ut mi tempus convallis nec eu ligula. Nunc eu fermentum quam. Mauris felis dui, commodo a commodo in, rhoncus a ex. Maecenas vel tortor blandit, semper augue id, ullamcorper odio. Nam in leo sit amet lacus fermentum ornare. Fusce dolor est, euismod pretium enim nec, eleifend condimentum velit. Aenean nec volutpat felis, sit amet tristique massa. Nullam ac est leo.",  "tests,debugging,design"),
-    new Entry(1,"test2", "lorem ipsum etc", 1, "tests,debugging,design")
+    new Entry(1,"test2", "lorem ipsum etc", "tests,debugging,design")
+    ,new Entry(21,"test2", "lorem ipsum etc", "tests,debugging,design")
+    ,new Entry(22,"test2", "lorem ipsum etc", "tests,debugging,design")
+    ,new Entry(23,"test2", "lorem ipsum etc", "tests,debugging,design")
 
   ]
